41f08d4cbcb8b97a3dce34d734b9d3b44127dd8c
Niklas Haiden <niklas.haiden@gmail.com>
2025-04-02 16:25:47 +0200

    add shelly plug exporter, change aurora text

--- components/sections/Projects.tsx
+++ components/sections/Projects.tsx
@@ -11,7 +11,9 @@ export default function Projects({projectRef}: {projectRef: RefObject<any>} ) {
                 </code>
                 <div className={"w-4/6 lg:w-1/2 grid grid-cols-1 gap-6 lg:p-4 mb-10 lg:mb-0"}>
                     <ProjectCard projectTitle={"Raven"} projectDescription={"Raven is an experiment of mine to build a personal PaaS platform, similar to Heroku or other services to deploy dockerized apps easily via a web interface or API. Currently work in progress."}/>
-                    <ProjectCard link={"https://getaurora.dev"} projectTitle={"Aurora"} projectDescription={"Aurora was born out of the need for a seamless Linux workstation experience with a beautiful desktop and every developer tool preinstalled, paired with unmatched stability. It's now part of Universal-Blue."}/>
+                    <ProjectCard projectTitle={"Shelly Plug Exporter"} projectDescription={"A small webapplication to export Shelly Plugs to Prometheus with a nice GUI. Coming soon."}/>
+
+                    <ProjectCard link={"https://getaurora.dev"} projectTitle={"Aurora"} projectDescription={"Aurora was born out of the need for a seamless Linux workstation experience with a beautiful desktop and every developer tool preinstalled, paired with unmatched stability. It is the ultimate developer workstation."}/>
                 </div>
             </div>
         </div>
